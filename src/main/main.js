8eb39700d704cb9f7b6f341a39cc79b7685bde94
kongxiangyan <kcigaret@outlook.com>
2021-03-11 14:33:26 +0800

    WIP: start template

--- src/main/main.js
+++ src/main/main.js
@@ -1,10 +1,24 @@
-// const { app, BrowserWindow } = require('electron')
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, nativeImage } from 'electron'
+import { cwd } from 'process'
+import path from 'path'
+
+export const ROOT = cwd()
+export const rootResolvePath = (...paths) => path.resolve(ROOT, ...paths)
 
 function createWindow () {
+  // TODO: Differentiate different environments
+  // refer: https://www.electronjs.org/docs/api/native-image
+  console.log(ROOT)
+  const iconPath = rootResolvePath('')
+
+  // refer: https://www.electronjs.org/docs/api/browser-window
   const win = new BrowserWindow({
     width: 1920,
     height: 1080,
+    icon: './statics/favicons/favicon.ico',
+    // frame: false,
+    // darkTheme: true,
+    // transparent: true,
     webPreferences: {
       nodeIntegration: false,
       webSecurity: true,
