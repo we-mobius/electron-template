8346f6b35897e1171b138857ed7d11bbcb87667a
kongxiangyan <kcigaret@outlook.com>
2021-03-12 16:07:54 +0800

    chore: electron pack script

---
+++ scripts/pack.js
@@ -0,0 +1,20 @@
+import packager from 'electron-packager'
+
+const pack = () => {
+  return packager({
+    appCopyright: 'Copyright © 2021 cigaret.',
+    appVersion: '0.1.0',
+    arch: ['x64', 'arm64'],
+    asar: true,
+    dir: './dist',
+    name: 'Mobius',
+    icon: './src/statics/favicons/favicon.ico',
+    out: 'packed',
+    overwrite: true,
+    platform: ['win32']
+  })
+}
+
+pack().then(res => {
+  console.log('[pack] complete! ', res)
+})
